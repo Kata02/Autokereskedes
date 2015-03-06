fdd2da1f9dba3c2715c8432edb955517d157fa76
Kata <kata.gallambos@gmail.com>
2015-03-06 09:23:06 +0100

    NhibernateDB

---
+++ DB_Nhibernate/DB_Nhibernate/Form1.cs
@@ -0,0 +1,29 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DB_Nhibernate
+{
+    public partial class Form1 : Form
+    {
+        private Configuration myConfiguration;
+        private ISessionFactory mySessionFactory;
+        private ISession mySession;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DB_Nhibernate/DB_Nhibernate/contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DB_Nhibernate
+{
+   public class contact
+    {
+       public virtual int emp_id {get; set;}
+       public virtual int emp_irszam { get; set; }
+       public virtual int emp_parkolohely{ get; set; }
+       public virtual string emp_telephely { get; set; }
+       public virtual string emp_adress { get; set; }
+
+       
+    }
+}
